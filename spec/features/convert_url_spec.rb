253648c4e6786e2a3f034963c2106ce723e68f5c
Harry Ng <harryworld@gmail.com>
2014-07-04 12:03:33 +0800

    Add UrlController

--- spec/features/convert_url_spec.rb
+++ spec/features/convert_url_spec.rb
@@ -7,6 +7,8 @@ feature 'visit my first page' do
       fill_in 'original', :with => 'http://www.google.com/'
       click_button 'Submit'
     end
+
     expect(current_path).to eq '/url'
+    expect(page).to have_content('Created URL')
   end
 end
