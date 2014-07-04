6f451dcd020429dac5fb46cc791b663b82898e6c
Harry Ng <harryworld@gmail.com>
2014-07-04 12:32:57 +0800

    Add the form to page

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe Url, :type => :model do
     expect(url).to be_valid
   end
 
+  it "is valid to have original set" do
+    url = Url.new
+    url.original = 'harry'
+    url.save
+
+    expect(url).to be_valid
+  end
+
   it "is invalid if original is not set" do
     url = Url.new
     url.save
